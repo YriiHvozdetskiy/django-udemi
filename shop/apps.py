256570d065f5c2b71e7cb6ef7ae4d7fe25113c80
rasty <hvozdetskiyyrii@gmail.com>
2024-02-01 14:52:43 +0200

    shop

---
+++ shop/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class ShopConfig(AppConfig):
+    # атрибути класа
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'shop'
+
+# ShopConfig.name # 'shop'
---
+++ shop/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+"""
+views - це контролер в патерні MVC
+звязує частину яку ми бачимо з models
+- виконують прийом запитів від користувача і передачу відповіді користувачу
+"""
+
+# Create your views here.
