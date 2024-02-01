a8040feae0c42d2fdfcdfb30f9d1056f20a6f342
rasty <hvozdetskiyyrii@gmail.com>
2024-02-01 19:33:05 +0200

    templates

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# from . import models  # імпортуємо з поточної папки весь модуль "models"
+# or
+from .models import Course, Category  # Імпортуємо конкретні модулі (. показує що це з поточної папки)
+
 """
-views - це контролер в патерні MVC
+views - це контролер/інтерфейс в патерні MVC
 звязує частину яку ми бачимо з models
 - виконують прийом запитів від користувача і передачу відповіді користувачу
 """
@@ -11,4 +15,7 @@ views - це контролер в патерні MVC
 # Create your views here.
 #  опрацьовуєм звернення на головну сторінку(shop)
 def index(request):  # функція виду
-    return HttpResponse('Hello from the Shop app')
+    courses = Course.objects.all()  # отримання всіх курсів
+    # return HttpResponse(''.join([str(course) + '<br>' for course in courses]))  # виводимо через br один під одним
+    # return HttpResponse(courses)
+    return render(request, 'courses.html', )
