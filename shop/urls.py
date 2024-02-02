de54363a1377593c6a67aaca61723163d89f1783
rasty <hvozdetskiyyrii@gmail.com>
2024-02-02 16:02:38 +0200

    route

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'shop'  # назва приложенія(використовується в навігації в templates)
+
 urlpatterns = [
     path('', views.index, name='index'),
     # course_id - називаєм як хочем параметр маршрута, берем ід з url в браузері
